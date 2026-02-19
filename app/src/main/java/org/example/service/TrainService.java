e0852b63ae5b6f44128c0dabbe21521881ad9447
nitinmehra09 <nitinmehra9983@gmail.com>
2026-02-19 22:16:06 +0530

    practice java using simple project

---
+++ app/src/main/java/org/example/service/TrainService.java
@@ -0,0 +1,39 @@
+package org.example.service;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.entities.Train;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+import java.util.OptionalInt;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class TrainService {
+
+    private List<Train> trainList;
+    private ObjectMapper objectMapper = new ObjectMapper();
+    private static final String TRAIN_DB_PATH = "../localDB/trains.json";
+
+    public TrainService() throws IOException {
+        File trains = new File(TRAIN_DB_PATH);
+        trainList = objectMapper.readValue(trains, new TypeReference<List<Train>>() {});
+    }
+
+    public List<Train> searchTrains(String source, String destination) {
+        return trainList.stream().filter(train -> validTrain(train, source, destination)).collect(Collectors.toList());
+    }
+
+    // done
+    private boolean validTrain(Train train, String source, String destination) {
+        List<String> stationOrder = train.getStations();
+
+        int sourceIndex = stationOrder.indexOf(source.toLowerCase());
+        int destinationIndex = stationOrder.indexOf(destination.toLowerCase());
+
+        return sourceIndex != -1 && destinationIndex != -1 && sourceIndex < destinationIndex;
+    }
+}
---
+++ app/src/main/java/org/example/service/UserBookingService.java
@@ -0,0 +1,93 @@
+package org.example.service;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.example.entities.Ticket;
+import org.example.entities.Train;
+import org.example.entities.User;
+import org.example.util.UserServiceUtil;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+
+public class UserBookingService{
+    
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    private List<User> userList;
+
+    private User user;
+
+    private final String USER_FILE_PATH = "app/src/main/java/org/example/localDb/users.json";
+
+    public UserBookingService(User user) throws IOException {
+        loadUserListFromFile();
+        this.user = user;
+        if(!loginUser()){
+            System.out.println("Login Failed : ");
+        }else {
+            System.out.println("Login successful");
+        }
+    }
+
+    public UserBookingService() throws IOException {
+        loadUserListFromFile();
+    }
+
+    private void loadUserListFromFile() throws IOException {
+        userList = objectMapper.readValue(new File(USER_FILE_PATH), new TypeReference<List<User>>() {});
+    }
+
+    public Boolean loginUser(){
+        Optional<User> foundUser = userList.stream().filter(user1 -> {
+            return user1.getName().equals(user.getName()) && UserServiceUtil.checkPassword(user.getPassword(), user1.getHashedPassword());
+        }).findFirst();
+        return foundUser.isPresent();
+    }
+
+    public Boolean signUp(User user1){
+        try{
+            userList.add(user1);
+            saveUserListToFile();
+            System.out.println("Account Created ");
+            return Boolean.TRUE;
+        }catch (IOException ex){
+            return Boolean.FALSE;
+        }
+    }
+
+    private void saveUserListToFile() throws IOException {
+        File usersFile = new File(USER_FILE_PATH);
+        objectMapper.writeValue(usersFile, userList);
+    }
+
+    public void fetchBookings(){
+        Optional<User> userFetched = userList.stream().filter(user1 -> {
+            return user1.getName().equals(user.getName()) && UserServiceUtil.checkPassword(user.getPassword(), user1.getHashedPassword());
+        }).findFirst();
+        if(userFetched.isPresent()){
+            userFetched.get().printTickets();
+        }else{
+            System.out.println("First login ");
+        }
+    }
+
+
+    public List<Train> getTrains(String source, String destination){
+        try{
+            TrainService trainService = new TrainService();
+            return trainService.searchTrains(source, destination);
+        }catch(IOException ex){
+            return new ArrayList<>();
+        }
+    }
+
+    public List<List<Integer>> fetchSeats(Train train){
+            return train.getSeats();
+    }
+
+}
